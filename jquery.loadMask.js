8af3db7086efc59767938abdf3e432f8d9675f35
Lyon <jrfanlin@gmail.com>
2015-08-21 11:22:47 +0800

    1.0

---
+++ jquery.loadMask.js
@@ -0,0 +1,48 @@
+/*!
+ * jQuery loadMask plugin
+ * Version 1.0
+ * Requires jQuery v1
+ *
+ * Examples at: https://github.com/lyonlin/jquery.loadMask
+ * Copyright (c) 2007-2013 M. Alsup
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Lyon Lin - 2015.08.21
+ */
+
+(function($) {
+
+    var init = function(el, opts) {
+    	$(el).addClass('loadMask').html('<div></div>');
+    	// 自定圖片
+    	if(opts.image!='')
+        {            
+    		$(el).find("div").css("background-image", "url(" + opts.image + ")");
+    		$(el).find("div").css("height", opts.height + "px");
+    		$(el).find("div").css("width", opts.width + "px");
+        }    
+    	// 自定背景色
+    	if(opts.bgcolor!='')
+        {            
+    		$(el).css("background-color", opts.bgcolor);
+        }
+        // 關閉MASK
+    	setTimeout(function(){ $(el).fadeOut(opts.speed); }, opts.timeout);
+    };
+    
+    $.fn.loadMask = function(opts) {
+		return this.each(function() {  
+			init(this,{
+				image : typeof opts.image  !== 'undefined' ? opts.image  : '',
+				height : typeof opts.height  !== 'undefined' ? opts.height  : 120,
+				width : typeof opts.width  !== 'undefined' ? opts.width  : 120,
+				bgcolor: typeof opts.bgcolor !== 'undefined' ? opts.bgcolor : '',
+				timeout: typeof opts.speed !== 'undefined' ? opts.speed : 500,
+				speed: typeof opts.speed !== 'undefined' ? opts.speed : 1000,				
+			});
+		});		
+    };
+
+})(jQuery);
