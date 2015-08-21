2d964bee2e8613bbd4ee04b96df4ebb66d2aa0ab
Lyon <jrfanlin@gmail.com>
2015-08-21 11:56:02 +0800

    1.1

--- jquery.loadMask.js
+++ jquery.loadMask.js
@@ -1,27 +1,31 @@
 /*!
  * jQuery loadMask plugin
- * Version 1.0 - 2015.08.21
+ * Version 1.1 - 2015.08.21
  * Requires jQuery v1
  *
  * Examples at: https://github.com/lyonlin/jquery.loadMask
  * Copyright (c) 2015 by Lyon Lin
  * Dual licensed under the MIT licenses:
  * http://www.opensource.org/licenses/mit-license.php
- *
  */
 
 (function($) {
 
     var init = function(el, opts) {
-    	$(el).addClass('loadMask').html('<div></div>');
-    	// 自定圖片
-    	if(opts.image!='')
+		$(el).addClass('loadMask').html('<div></div>');
+    	// 圖片停用
+		if(opts.image=='none')
+    	{
+    		$(el).css('cursor', 'wait').html('');
+    	}
+		// 自訂圖片
+    	else if(opts.image!='')
         {            
     		$(el).find("div").css("background-image", "url(" + opts.image + ")");
     		$(el).find("div").css("height", opts.height + "px");
     		$(el).find("div").css("width", opts.width + "px");
-        }    
-    	// 自定背景色
+        }  		
+    	// 自訂背景色
     	if(opts.bgcolor!='')
         {            
     		$(el).css("background-color", opts.bgcolor);
